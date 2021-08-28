3154701a6a3517f0fbd08aae8358f82797f7167b
Harry <gurikin@yandex.ru>
2021-08-28 23:29:38 +0300

    exchanger remove classes not bind with exchanger example

--- src/main/java/org/gurikin/ThreadDemo.java
+++ src/main/java/org/gurikin/ThreadDemo.java
@@ -3,14 +3,8 @@ package org.gurikin;
 public class ThreadDemo {
     public static void main(String[] args) {
         // ==================================
-        // Example of wait/notify and producer/consumer behavior
+        // Example of exchanger
         // ==================================
-        ConsumerProducerWaitNotifyExample consumerProducerExample = new ConsumerProducerWaitNotifyExample();
-        int iterNums = 5;
-        int cnt = 0;
-        while (cnt++ < iterNums) {
-            new Thread(() -> consumerProducerExample.consumer()).start();
-            new Thread(() -> consumerProducerExample.producer()).start();
-        }
+        
     }
 }
