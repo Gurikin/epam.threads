1b26d3f25173a8abe6544efa77a4a7150f54db5b
biv <gurikin@yandex.ru>
2021-08-28 12:05:49 +0300

    wait-notify-consumer-producer make consumer-producer example

--- src/main/java/org/gurikin/ThreadDemo.java
+++ src/main/java/org/gurikin/ThreadDemo.java
@@ -1,22 +1,16 @@
 package org.gurikin;
 
-import java.util.concurrent.TimeUnit;
-
 public class ThreadDemo {
     public static void main(String[] args) {
         // ==================================
-        // Example of wait/notify
+        // Example of wait/notify and producer/consumer behavior
         // ==================================
-        Payment payment = new Payment();
-        for (int i = 0; i < 5; i++) {
-            final int j = i;
-            new Thread(() -> payment.doPayment("pay-" + j)).start();
+        ConsumerProducerWaitNotifyExample consumerProducerExample = new ConsumerProducerWaitNotifyExample();
+        int iterNums = 5;
+        int cnt = 0;
+        while (cnt++ < iterNums) {
+            new Thread(() -> consumerProducerExample.consumer()).start();
+            new Thread(() -> consumerProducerExample.producer()).start();
         }
-        try {
-            TimeUnit.MILLISECONDS.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        payment.init();
     }
 }
